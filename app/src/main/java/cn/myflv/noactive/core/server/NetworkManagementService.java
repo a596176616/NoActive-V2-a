47ff61fd19fef5ffd6f8b0612526a1514ab313ea
a596176616 <52849384+a596176616@users.noreply.github.com>
2026-07-20 12:48:42 +0800

    refactor(api102): stage 2-10 NetworkManagementService migrate to ReflectionUtils
    - Replace 7x XposedHelpers calls with ReflectionUtils:
      - 1x getObjectField (mNetdService)
      - 1x findClass (UidRangeParcel)
      - 1x callMethod (socketDestroy)
      - 2x newInstance (UidRangeParcel constructor)
      - 2x setObjectField (start, stop)
    - Compilation errors: 50 -> 49 (-1)

--- app/src/main/java/cn/myflv/noactive/core/server/NetworkManagementService.java
+++ app/src/main/java/cn/myflv/noactive/core/server/NetworkManagementService.java
@@ -10,7 +10,7 @@ import cn.myflv.noactive.constant.FieldConstants;
 import cn.myflv.noactive.constant.MethodConstants;
 import cn.myflv.noactive.core.entity.AppInfo;
 import cn.myflv.noactive.core.utils.Log;
-import de.robv.android.xposed.XposedHelpers;
+import cn.myflv.noactive.utils.ReflectionUtils;
 import lombok.Data;
 
 @Data
@@ -27,8 +27,8 @@ public class NetworkManagementService {
     public NetworkManagementService(ClassLoader classLoader, Object networkManagementService) {
         this.classLoader = classLoader;
         this.networkManagementService = networkManagementService;
-        this.mNetdService = XposedHelpers.getObjectField(networkManagementService, FieldConstants.mNetdService);
-        this.UidRangeParcel = XposedHelpers.findClass(ClassConstants.UidRangeParcel, classLoader);
+        this.mNetdService = ReflectionUtils.getObjectField(networkManagementService, FieldConstants.mNetdService);
+        this.UidRangeParcel = ReflectionUtils.findClass(ClassConstants.UidRangeParcel, classLoader);
     }
 
     /**
@@ -44,7 +44,7 @@ public class NetworkManagementService {
             Object uidRangeParcel = makeUidRangeParcel(uid, uid);
             Object uidRangeParcels = Array.newInstance(UidRangeParcel, 1);
             Array.set(uidRangeParcels, 0, uidRangeParcel);
-            XposedHelpers.callMethod(mNetdService, MethodConstants.socketDestroy, uidRangeParcels, new int[0]);
+            ReflectionUtils.callMethod(mNetdService, MethodConstants.socketDestroy, uidRangeParcels, new int[0]);
             Log.d(appInfo.getKey() + " socket destroyed");
         } catch (Throwable throwable) {
             Log.e("socketDestroy", throwable);
@@ -54,11 +54,11 @@ public class NetworkManagementService {
     private Object makeUidRangeParcel(int start, int stop) {
         Object uidRangeParcel;
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            uidRangeParcel = XposedHelpers.newInstance(UidRangeParcel, start, stop);
+            uidRangeParcel = ReflectionUtils.newInstance(UidRangeParcel, start, stop);
         } else {
-            uidRangeParcel = XposedHelpers.newInstance(UidRangeParcel);
-            XposedHelpers.setObjectField(uidRangeParcel, FieldConstants.start, start);
-            XposedHelpers.setObjectField(uidRangeParcel, FieldConstants.stop, stop);
+            uidRangeParcel = ReflectionUtils.newInstance(UidRangeParcel);
+            ReflectionUtils.setObjectField(uidRangeParcel, FieldConstants.start, start);
+            ReflectionUtils.setObjectField(uidRangeParcel, FieldConstants.stop, stop);
         }
         return uidRangeParcel;
     }
