659d373c52ac9a98faf6bad563ba606f95fdcc99
a596176616 <52849384+a596176616@users.noreply.github.com>
2026-07-20 12:17:29 +0800

    refactor(api102): stage 2-4 AlarmMangerService migrate to ReflectionUtils
    - Replace 1x XposedHelpers.getObjectField with ReflectionUtils.getObjectField
    - Replace 2x XposedHelpers.callMethod with ReflectionUtils.callMethod
    - Compilation errors: 56 -> 55 (-1)

--- app/src/main/java/cn/myflv/noactive/core/server/AlarmMangerService.java
+++ app/src/main/java/cn/myflv/noactive/core/server/AlarmMangerService.java
@@ -6,7 +6,7 @@ import cn.myflv.noactive.constant.FieldConstants;
 import cn.myflv.noactive.constant.MethodConstants;
 import cn.myflv.noactive.core.entity.AppInfo;
 import cn.myflv.noactive.core.utils.Log;
-import de.robv.android.xposed.XposedHelpers;
+import cn.myflv.noactive.utils.ReflectionUtils;
 import lombok.Data;
 
 @Data
@@ -16,16 +16,16 @@ public class AlarmMangerService {
 
     public AlarmMangerService(Object instance) {
         this.instance = instance;
-        lock = XposedHelpers.getObjectField(instance, FieldConstants.mLock);
+        lock = ReflectionUtils.getObjectField(instance, FieldConstants.mLock);
     }
 
 
     public void remove(AppInfo appInfo, int uid) {
         synchronized (lock) {
             if (Build.VERSION.SDK_INT < Build.VERSION_CODES.S) {
-                XposedHelpers.callMethod(instance, MethodConstants.removeLocked, uid);
+                ReflectionUtils.callMethod(instance, MethodConstants.removeLocked, uid);
             } else {
-                XposedHelpers.callMethod(instance, MethodConstants.removeLocked, uid, 0);
+                ReflectionUtils.callMethod(instance, MethodConstants.removeLocked, uid, 0);
             }
             Log.d(appInfo.getKey() + " alarm removed");
         }
