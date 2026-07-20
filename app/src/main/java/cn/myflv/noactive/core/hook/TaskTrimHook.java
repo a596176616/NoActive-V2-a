326fc1b2d2453efb9dab19b3e54f45566c5b53cb
a596176616 <52849384+a596176616@users.noreply.github.com>
2026-07-20 22:49:30 +0800

    refactor(api102): stage 3-15 TaskTrimHook migrate to API 102

--- app/src/main/java/cn/myflv/noactive/core/hook/TaskTrimHook.java
+++ app/src/main/java/cn/myflv/noactive/core/hook/TaskTrimHook.java
@@ -5,7 +5,7 @@ import cn.myflv.noactive.constant.MethodConstants;
 import cn.myflv.noactive.core.hook.base.AbstractReplaceHook;
 import cn.myflv.noactive.core.hook.base.MethodHook;
 import cn.myflv.noactive.core.utils.Log;
-import de.robv.android.xposed.XC_MethodHook;
+import io.github.libxposed.api.XposedInterface;
 
 public class TaskTrimHook extends MethodHook {
 
@@ -29,10 +29,10 @@ public class TaskTrimHook extends MethodHook {
     }
 
     @Override
-    public XC_MethodHook getTargetHook() {
+    public XposedInterface.Hooker getTargetHook() {
         return new AbstractReplaceHook() {
             @Override
-            protected Object replaceMethod(MethodHookParam param) throws Throwable {
+            protected Object replaceMethod(XposedInterface.Chain chain) throws Throwable {
                 Log.d("avoid trim inactive recent tasks");
                 return null;
             }
