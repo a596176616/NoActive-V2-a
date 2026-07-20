a596176616/NoActive-V2-a

commit 8fa7a3be5e0844407efec60f3d1a2782a896b034

build: upgrade blockmiui module + gradlew

gradlew
@@ -174,7 +174,7 @@ fi
 
 # Escape application args
 save () {
-    for i do printf %s\\n "$i" | sed "s/'/'\\\\''/g;1s/^/'/;\$s/\$/' \\\\/" ; done
+    for i do printf %s\\n "$i" | sed "s/'/'\\\\''/g;1s/^/'/;\$s/\$/ '\\\\" ; done
     echo " "
 }
 APP_ARGS=`save "$@"`

blockmiui/build.gradle.kts
@@ -4,11 +4,12 @@ plugins {
 }
 
 android {
-    compileSdk = 32
+    namespace = "cn.fkj233.miui"
+    compileSdk = 36
 
     defaultConfig {
         minSdk = 26
-        targetSdk = 32
+        targetSdk = 36
     }
 
     buildTypes {
@@ -32,5 +33,5 @@ android {
 }
 
 dependencies {
-    implementation("androidx.annotation:annotation:1.3.0")
+    implementation("androidx.annotation:annotation:1.10.0")
 }

blockmiui/src/main/AndroidManifest.xml
@@ -1,2 +1,2 @@
 <?xml version="1.0" encoding="utf-8"?>
-<manifest package="cn.fkj233.miui" />
+<manifest />

commit 193788d136067ca0973d884d0ff16c2f7caaa020

build: fix gradlew sed command broken in 8fa7a3b
Commit 8fa7a3b introduced a typo in the gradlew save() function's sed
expression: the trailing `\/` was lost, producing an unterminated `s`
command (`sed: -e expression #1, char 29: unterminated 's' command`).

This broke APP_ARGS escaping for any task argument containing quotes,
and (combined with an unrelated JAVA_HOME mismatch) blocked local
compilation. Restore the original expression so wrapper argument
quoting works again.

gradlew
@@ -174,7 +174,7 @@ fi
 
 # Escape application args
 save () {
-    for i do printf %s\\n "$i" | sed "s/'/'\\\\''/g;1s/^/'/;\$s/\$/ '\\\\" ; done
+    for i do printf %s\\n "$i" | sed "s/'/'\\\\''/g;1s/^/'/;\$s/\$/' \\\\/" ; done
     echo " "
 }
 APP_ARGS=`save "$@"`